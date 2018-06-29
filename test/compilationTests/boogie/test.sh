a3c9a9e6567df5c73e1669db48f2195422af0e22
Akos Hajdu <akos.hajdu@sri.com>
2018-06-29 10:41:49 -0400

    Add expected results to tests

--- test/compilationTests/boogie/test.sh
+++ test/compilationTests/boogie/test.sh
@@ -3,23 +3,41 @@
 BOOGIE=~/Workspace/boogie/Binaries/Boogie.exe
 CORRAL=~/Workspace/corral/bin/Debug/corral.exe
 
+total=0
+passed=0
+failed=0
+
 mkdir output
-for f in *.sol; do
-    solc --boogie "$f" -o ./output --overwrite
-done
-
-echo -e "\n======= Running verifier =======\n"
-
-for f in ./output/*.bpl; do
-    grep "main\(\)" "$f" > /dev/null
+for sol in *.sol; do
+    total=$((total+1))
+    echo "======= $sol ======="
+    echo "- Compiling"
+    solc --boogie "$sol" -o ./output --overwrite > /dev/null
+    bpl="./output/$sol.bpl"
+    out="$bpl.out"
+    exp="$sol.expected"
+    grep "main\(\)" "$bpl" > /dev/null
     if [[ "$?" == "0" ]] 
     then
-        echo -e "\n======= $f (with main function, running Corral) =======\n"
-        mono $CORRAL "$f" /main:main /recursionBound:20
+        echo -e "- Running Corral (main found)"
+        mono $CORRAL "$bpl" /main:main /recursionBound:20 > "$out"
     else
-        echo -e "\n======= $f (without main function, running Boogie) =======\n"
-        mono $BOOGIE "$f" /doModSetAnalysis /nologo /errorTrace:0
+        echo -e "- Running Boogie (no main)"
+        mono $BOOGIE "$bpl" /doModSetAnalysis /nologo /errorTrace:0 > "$out"
+    fi
+
+    grep -f "$exp" "$out" > /dev/null
+    if [[ "$?" == "0" ]] 
+    then
+        echo -e "- Output OK"
+        passed=$((passed+1))
+    else
+        echo -e "- Error in output"
+        failed=$((failed+1))
     fi
 done
 
 rm -rf output
+
+echo "======= Done ======="
+echo "$total tests, $passed passed, $failed failed"
