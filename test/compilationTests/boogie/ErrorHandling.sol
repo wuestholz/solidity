93841fcb473311b0211927464a8e99e78d6e4074
Akos Hajdu <akos.hajdu@sri.com>
2018-07-05 22:19:22 -0400

    Merge error handling tests

---
+++ test/compilationTests/boogie/ErrorHandling.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.23;
+
+contract ErrorHandling {
+    
+    // Assertion should hold
+    function abs_sound(int param) pure public returns (int) {
+        int result = param;
+        if (result < 0) result *= -1;
+        assert(result >= 0);
+        return result;
+    }
+
+    // Assertion should fail
+    function abs_unsound(int param) pure public returns (int) {
+        int result = param;
+        if (result < 0) result += -1;
+        assert(result >= 0);
+        return result;
+    }
+
+    // Assertion should hold given the requirements
+    function add(int x, int y) pure public returns (int) {
+        require(x > 0);
+        require(y > 0);
+        int result = x + y;
+        assert(result > x);
+        assert(result > y);
+        return result;
+    }
+
+    // Assertion should hold due to revert
+    function add2(int x, int y) pure public returns (int) {
+        if (x <= 0 || y <= 0) revert();
+        int result = x + y;
+        assert(result > x);
+        assert(result > y);
+        return result;
+    }
+}
