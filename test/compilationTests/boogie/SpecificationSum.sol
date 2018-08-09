e799f5509482d4b29ec8c2787ef9479c7face9af
Akos Hajdu <akos.hajdu@sri.com>
2018-08-09 09:10:46 -0400

    Reduce int size in example

--- test/compilationTests/boogie/SpecificationSum.sol
+++ test/compilationTests/boogie/SpecificationSum.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.23;
 
 /**
- * @notice invariant __verifier_sum_int32(user_balances) == total_supply
+ * @notice invariant __verifier_sum_uint8(user_balances) == total_supply
  */
 contract SomeContract {
-    int32 total_supply;
-    mapping(address=>int32) user_balances;
+    uint8 total_supply;
+    mapping(address=>uint8) user_balances;
 
-    function deposit(int32 amount) public {
+    function deposit(uint8 amount) public {
         total_supply += amount;
         user_balances[msg.sender] += amount;
     }
@@ -19,6 +19,6 @@ contract SomeContract {
 
     function withdraw_incorrect() public {
         total_supply -= user_balances[msg.sender];
-        user_balances[msg.sender] = 12345;
+        user_balances[msg.sender] = 12;
     }
 }
