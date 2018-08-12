183fac0219894054f1bfc6f8c0c37dc96e707902
Akos Hajdu <akos.hajdu@sri.com>
2018-08-11 22:57:39 -0400

    Add demo examples

--- test/compilationTests/boogie/demo/DemoAssertion.sol
+++ test/compilationTests/boogie/demo/DemoAssertion.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.23;
 
 contract DemoAssertion {
-    function abs(int param) pure public returns (int) {
+    function abs(int param) public pure returns (int) {
         int result = param;
         if (result < 0) result *= -1;
         assert(result >= 0);
