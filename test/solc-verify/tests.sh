131ec98b52fa91de2848d48595fcdd954fd89a01
Dejan Jovanovic <dejan.jovanovic@sri.com>
2019-04-15 14:29:37 -0400

    update testing script to run all tests even when failing

--- test/solc-verify/tests.sh
+++ test/solc-verify/tests.sh
@@ -26,95 +26,102 @@
 # (c) 2016 solidity contributors.
 #------------------------------------------------------------------------------
 
-set -e
-
 ## GLOBAL VARIABLES
 
 SOLCVERIFY_TESTS=$(cd $(dirname "$0") && pwd)
 REPO_ROOT=$(cd $(dirname "$0")/../.. && pwd)
 SOLCVERIFY="$REPO_ROOT/build/solc/solc-verify.py"
 
-## FUNCTIONS
-
-if [ "$CIRCLECI" ]
-then
-    function printTask() { echo "$(tput bold)$(tput setaf 2)$1$(tput setaf 7)"; }
-    function printError() { echo "$(tput setaf 1)$1$(tput setaf 7)"; }
-else
-    function printTask() { echo "$(tput bold)$(tput setaf 2)$1$(tput sgr0)"; }
-    function printError() { echo "$(tput setaf 1)$1$(tput sgr0)"; }
+## COLORS
+RED=
+GREEN=
+BLACK=
+if test -t 1 ; then
+  RED=$(tput setaf 1)
+  GREEN=$(tput setaf 2)
+  BLACK=$(tput sgr0)
 fi
 
-# General helper function for testing SOLC-VERIFY behaviour, based on file name, compile opts, exit code, stdout and stderr.
-# An failure is expected.
-function test_solcverify_behaviour()
-{
-    local filename="${1}"
-    local solcverify_args="${2}"
-    local solcverify_stdin="${3}"
-    local stdout_expected="${4}"
-    local exit_code_expected="${5}"
-    local stderr_expected="${6}"
-    local stdout_path=`mktemp`
-    local stderr_path=`mktemp`
-    if [[ "$exit_code_expected" = "" ]]; then exit_code_expected="0"; fi
+# Time format
+TIMEFORMAT="%U"
 
-    set +e
-    if [[ "$solc_stdin" = "" ]]
-    then
-        "$SOLCVERIFY" "${filename}" ${solc_args} 1>$stdout_path 2>$stderr_path
-    else
-        "$SOLCVERIFY" "${filename}" ${solc_args} <$solc_stdin 1>$stdout_path 2>$stderr_path
-    fi
-    exitCode=$?
-    set -e
+## Keep track of failed tests
+FAIL=0
+PASS=0
+FAILED_TESTS=()
 
-    if [[ $exitCode -ne "$exit_code_expected" ]]
-    then
-        printError "Incorrect exit code. Expected $exit_code_expected but got $exitCode."
-        rm -f $stdout_path $stderr_path
-        exit 1
-    fi
-
-    if [[ "$(cat $stdout_path)" != "${stdout_expected}" ]]
-    then
-        printError "Incorrect output on stdout received. Expected:"
-        echo -e "${stdout_expected}"
-
-        printError "But got:"
-        cat $stdout_path
-        printError "When running $SOLC ${filename} ${solc_args} <$solc_stdin"
-
-        rm -f $stdout_path $stderr_path
-        exit 1
-    fi
-
-    if [[ "$(cat $stderr_path)" != "${stderr_expected}" ]]
-    then
-        printError "Incorrect output on stderr received. Expected:"
-        echo -e "${stderr_expected}"
-
-        printError "But got:"
-        cat $stderr_path
-        printError "When running $SOLC ${filename} ${solc_args} <$solc_stdin"
-
-        rm -f $stdout_path $stderr_path
-        exit 1
-    fi
-
-    rm -f $stdout_path $stderr_path
+## Success/fail printout
+function reportError() {
+    test_name=$1
+    elapsed=$2
+    message=$3
+    echo -n "$test_name "
+    echo -n $RED
+    echo FAIL [$elapsed s]
+	echo -n $BLACK
+    FAIL=$((FAIL+1))
+    FAILED_TESTS+=("$test_name"$'\n'"$message")
+}
+function reportSuccess() {
+    test_name=$1
+    elapsed=$2
+    message=$3
+    echo -n "$test_name "
+    echo -n $GREEN
+    echo PASS [$elapsed s]
+    echo -n $black
+    PASS=$((PASS+1))
 }
 
+# Temp files
+OUT_PATH=`mktemp`
+TIME_PATH=`mktemp`
+
+# Run SOLC-VERIFY and check result based on file, arguments and expected output
+function solcverify_check()
+{
+    filename="${1}"
+    solcverify_args="${2}"
+    out_expected="${3}"
+   
+    # Test id
+    test_string="$filename"
+    [[ !  -z  $solcverify_args  ]] && test_string="$test_string [ $solcverify_args ]"
+
+    # Run it
+    (time "$SOLCVERIFY" "${filename}" ${solc_args} >& $OUT_PATH) >& $TIME_PATH
+    elapsed=$(cat $TIME_PATH)
+    
+    # Check output
+    out_diff=$(diff -w $out_expected $OUT_PATH)
+    if [ $? -ne 0 ]
+    then
+        reportError "$test_string" "$elapsed" "$out_diff"
+    else
+        reportSuccess "$test_string" "$elapsed"
+    fi
+}
+
+# Run the tests
 for filename in $SOLCVERIFY_TESTS/*.sol; do
+    # Flags to use
     flags=""
     if [ -f "$filename.flags" ]; then
         flags=$(<"$filename.flags")
     fi
-	if [ -f "filename.gold" ]; then 
-		gold=$(<"$filename.gold")
-    fi
-    exp="$sol.expected"
-	test_solcverify_behaviour "$filename" "$flags" "" "$gold" 
+    # Run the test
+	solcverify_check "$filename" "$flags" "$filename.gold" 
 done
 
-cleanup
+# Remove temps
+rm -f $OUT_PATH $TIME_PATH 
+
+# Print the details of the failed tests
+if [ $FAIL -eq 0 ]
+then
+    exit 0
+else
+	for i in "${!FAILED_TESTS[@]}"; do echo "$((i+1)). ${FAILED_TESTS[$i]}"; done
+    exit 1
+fi
+
