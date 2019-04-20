210d211c0006f9600e4abb50620fff89327f95e4
Dejan Jovanovic <dejan.jovanovic@sri.com>
2019-04-20 11:52:02 -0400

    ah once more

--- test/solc-verify/test_with_truffle.sh
+++ test/solc-verify/test_with_truffle.sh
@@ -32,6 +32,7 @@ REPO_ROOT=$(cd $(dirname "$0")/../.. && pwd)
 SOLCVERIFY_TESTS="test/solc-verify"
 SOLC_BIN="$REPO_ROOT/build/solc"
 
+source ~/.nvm/nvm.sh
 nvm use node
 
 cd $REPO_ROOT/$SOLCVERIFY_TESTS
