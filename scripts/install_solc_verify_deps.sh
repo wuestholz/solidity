37f507ea4e0eb4ae8caed2661b2cf5eaba68bae2
Dejan Jovanovic <dejan.jovanovic@sri.com>
2019-04-24 10:51:55 -0400

    [travis] retry the nuget, seems to be flaky

--- scripts/install_solc_verify_deps.sh
+++ scripts/install_solc_verify_deps.sh
@@ -29,7 +29,7 @@ pushd .
 git clone https://github.com/dddejan/boogie.git
 cd boogie
 wget https://nuget.org/nuget.exe
-mono ./nuget.exe restore Source/Boogie.sln
+travis_retry mono ./nuget.exe restore Source/Boogie.sln
 xbuild Source/Boogie.sln
 popd
 
