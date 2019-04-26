624bfb31d605baf2b60713c3804922d3714c3450
Dejan Jovanovic <dejan.jovanovic@sri.com>
2019-04-26 15:14:01 -0400

    more

--- test/solc-verify/Tuples.sol
+++ test/solc-verify/Tuples.sol
@@ -1,14 +1,30 @@
 pragma solidity >=0.5.0;
 
 contract Tuples {
-   
+
+    function testSideffect() public pure returns (bool) {
+      int a = 0;
+      int x = 0;
+      int y = 0;
+      (x, y) = (++ a /* 1 */, (++ x) + a /** 2 */);
+      // Not correct:
+      // x = ++ a;
+      // y = x + a;
+      assert(a == 1);
+      assert(x == 1);
+      assert(y == 2);
+      return true;
+    }
+
     function minMax(int x, int y) private pure returns (int min, int max) {
       if (x > y) {
         min = y;
         max = x;
-      } else {
+      } else if (x < y) {
         min = x;
         max = y;
+      } else {
+        return (0, 0);
       }
     }
 
@@ -17,6 +33,7 @@ contract Tuples {
       int y = 20;
       (int min, int max) = minMax(x, y);
       assert(min == 10 && max == 20);
+      testSideffect();
     }
     
 }
