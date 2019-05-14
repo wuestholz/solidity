508170c7ecaa49f1d40307fa54f082272c87732d
Akos Hajdu <akos.hajdu@sri.com>
2019-05-14 09:41:00 -0400

    Separate test cases

--- test/solc-verify/StructsMem.sol
+++ test/solc-verify/StructsMem.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.5.0;
 
-contract StructsSimple {
+contract StructsMem {
     struct S {
         int x;
         bool y;
@@ -11,16 +11,13 @@ contract StructsSimple {
         int z;
     }
 
-    S s1;
-
     function () external {
-        s1.x = 5;
-        s1.t.z = 8;
-        assert(s1.x == 5);
-        assert(s1.t.z == 8);
-
         T memory tm = T(2);
         S memory sm = S(1, true, tm);
         assert(sm.t.z == 2);
+
+        S memory sm2 = sm;
+        sm2.t.z = 5;
+        assert(sm.t.z == 5);
     }
 }
---
+++ test/solc-verify/StructsNestedAccess.sol
@@ -0,0 +1,32 @@
+pragma solidity >=0.5.0;
+
+contract StructsNestedAccess {
+    struct S {
+        int x;
+        bool y;
+        T t;
+    }
+
+    struct T {
+        int z;
+        mapping(address=>U) u;
+    }
+
+    struct U {
+        int v;
+    }
+
+    mapping(address=>S) ss;
+
+    function () external {
+        ss[msg.sender].x = 1;
+        ss[msg.sender].y = true;
+        ss[msg.sender].t.z = 2;
+        ss[msg.sender].t.u[address(this)].v = 3;
+
+        assert(ss[msg.sender].x == 1);
+        assert(ss[msg.sender].y == true);
+        assert(ss[msg.sender].t.z == 2);
+        assert(ss[msg.sender].t.u[address(this)].v == 3);
+    }
+}
