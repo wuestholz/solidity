dc8666d7d91c171cadb37ddf67765ab783f8422d
Dejan Jovanovic <dejan.jovanovic@sri.com>
2019-05-14 14:38:49 -0400

    more details

--- test/solc-verify/ArrayLayout.sol
+++ test/solc-verify/ArrayLayout.sol
@@ -6,10 +6,10 @@ contract ArrayLayout {
     int x;
   }
 
-  S[] m_a;
+  S[] m_a; // No aliasing inside
 
   function() external payable {
-    S[] memory a;
+    S[] memory a; // Memory array can have pointers inside
     S memory f = S(1);
  
     a = new S[](2);
@@ -37,5 +37,30 @@ contract ArrayLayout {
     a0.x = 3;
     assert(m_a[0].x == 2);
     assert(m_a[1].x == 3);
+
+    a = m_a; // Creates a new array and assignes m_a to it
+    assert(a[0].x == 2);
+    assert(a[1].x == 3);
+
+    S[][] memory a2 = new S[][](2);
+    a2[0] = a; // Reference to a
+    a2[1] = a; // Reference to a
+    assert(a2[0][0].x == 2 && a2[0][1].x == 3);
+    assert(a2[1][0].x == 2 && a2[1][1].x == 3);
+    a[0].x = 1; // Changing both elements of a2? YES.
+    assert(a2[0][0].x == 1 && a2[0][1].x == 3);
+    assert(a2[1][0].x == 1 && a2[1][1].x == 3);
+
+    m_a[0].x = 0;
+    m_a[1].x = 0;
+    a2[1] = m_a; // Change a? NO. Change both a2[0]? NO.
+    assert(a[0].x == 1 && a[1].x == 3);
+    assert(a2[0][0].x == 1 && a2[0][1].x == 3);
+    assert(a2[1][0].x == 0 && a2[1][1].x == 0);
+  
+    assembly {
+      
+    }
+ 
   }
 }
