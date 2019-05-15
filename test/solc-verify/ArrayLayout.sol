750f9c0960cd3212635b6cc251bd10942f0ac3fe
Akos Hajdu <akos.hajdu@sri.com>
2019-05-15 17:49:42 -0400

    Remove trailing whitespace

--- test/solc-verify/ArrayLayout.sol
+++ test/solc-verify/ArrayLayout.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.5.0;
 
 contract ArrayLayout {
-  
+
   struct S {
     int x;
   }
@@ -11,13 +11,13 @@ contract ArrayLayout {
   function() external payable {
     S[] memory a; // Memory array can have pointers inside
     S memory f = S(1);
- 
+
     a = new S[](2);
     a[0] = f;
     a[1] = f;
     // Both array elements are memory references
     assert(a[0].x == a[1].x);
- 
+
     a[0].x = 2;
     // Since they are both references, they are still same
     assert(a[0].x == a[1].x);
@@ -26,7 +26,7 @@ contract ArrayLayout {
     m_a.push(m_a[0]);
     // Storage arrays are no-alias ATDs, no references, copying
     assert(m_a[0].x == m_a[1].x);
-    
+
     m_a[0].x = 2;
     // We change copies, so different
     assert(m_a[0].x != m_a[1].x);
@@ -57,10 +57,10 @@ contract ArrayLayout {
     assert(a[0].x == 1 && a[1].x == 3);
     assert(a2[0][0].x == 1 && a2[0][1].x == 3);
     assert(a2[1][0].x == 0 && a2[1][1].x == 0);
-  
+
     assembly {
-      
+
     }
- 
+
   }
 }
