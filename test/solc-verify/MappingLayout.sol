bef26e9d715b01913780be9afeb0eb2a3ec1d2c2
Akos Hajdu <akos.hajdu@sri.com>
2019-05-16 15:12:14 -0400

    Add test for mapping layout

---
+++ test/solc-verify/MappingLayout.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.5.0;
+
+contract Layout {
+
+
+    struct T {
+        int z;
+    }
+
+    mapping(address=>T) ts;
+
+    function() external payable {
+        ts[msg.sender] = T(1);
+        ts[address(this)] = ts[msg.sender];
+
+        assert(ts[address(this)].z == 1);
+        ts[msg.sender].z = 2;
+        assert(ts[address(this)].z == 1);
+    }
+}
