6e71acf51e43cdde13dac5358164766d1f1fb9dd
Akos Hajdu <akos.hajdu@sri.com>
2019-06-05 17:19:37 -0400

    More meaningful error message for base constructor call

--- libsolidity/ast/ASTBoogieConverter.cpp
+++ libsolidity/ast/ASTBoogieConverter.cpp
@@ -517,6 +517,8 @@ void ASTBoogieConverter::processModifier()
 			m_currentBlocks.top()->addStmt(boogie::Stmt::comment("Inlined modifier " + modifierDecl->name() + " ends here"));
 			m_currentReturnLabel = oldReturnLabel;
 		}
+		else if(dynamic_cast<ContractDefinition const*>(modifier->name()->annotation().referencedDeclaration))
+			m_context.reportError(&*modifier, "Base constructor call is not supported as modifier invocation");
 		else
 			m_context.reportError(&*modifier, "Unsupported modifier invocation");
 	}
