af220cbe2768d9560736cde37371836db1340c35
Dejan Jovanovic <dejan.jovanovic@sri.com>
2019-06-13 11:25:32 -0400

    a bit more complicated, works!

--- test/solc-verify/ModifiersDuplicate.sol
+++ test/solc-verify/ModifiersDuplicate.sol
@@ -9,14 +9,14 @@ contract ModifiersDuplicate {
         x += y;
     }
 
-    /** @notice postcondition x == __verifier_old_uint(x) + 7 */
-    function test() public incr(1) incr(2) {
+    /** @notice postcondition x == 3*__verifier_old_uint(x) + 13 */
+    function test() public incr(1) incr(2) incr(x) {
         x++;
     }
 
     function() external payable {
         x = 0;
         test();
-        assert(x == 7);
+        assert(x == 13);
     }
 }
