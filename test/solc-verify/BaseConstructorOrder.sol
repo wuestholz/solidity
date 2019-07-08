6d236c2a835188cd8c041a669be3e2ce471b6fdf
Dejan Jovanovic <dejan.jovanovic@sri.com>
2019-07-08 14:11:27 -0400

    fixing up the test case for another corner case of #34

--- test/solc-verify/BaseConstructorOrder.sol
+++ test/solc-verify/BaseConstructorOrder.sol
@@ -2,22 +2,35 @@ pragma solidity >=0.5.0;
 
 contract A {
     int public x;
-    constructor(int _x) public { x = _x; }
+    /// @notice postcondition (x == __verifier_old_int(_x))
+    function set(int _x) public { x = _x; }
+    constructor(int _x) public { set(_x); }
 }
 
 contract B1 is A {
-  constructor(int _x) public A(_x) { x += _x; }
+  constructor(int _x) public A(_x) { set(x*_x); }
 }
 
 contract B2 is A {
-  constructor(int _x) public { x = _x; }
+  constructor(int _x) public { set(x+_x); }
 }
 
-contract BaseConstructorOrder is B2, B1 {
+contract BaseConstructorOrder is B1, B2 {
 
-    constructor() public B1(2) B2(2) {
-        assert(x == 4);
-    }
+  modifier m(int _x) {
+    _;
+    set(_x);
+    assert(x == 0);
+  }
 
-    function() external payable { } // Needed for detecting as a truffle test case
+  modifier m1(A a) {
+    _;
+    a.set(0);
+  }
+
+  constructor() public B1(x+2) B2(x) m(x) {
+    assert(x == 4);
+  }
+
+  function() external payable { } // Needed for detecting as a truffle test case
 }
