79ebd3138ce0788d39a7dc0e0ee1f7501892fb75
Akos Hajdu <akos.hajdu@sri.com>
2019-07-12 16:06:58 -0400

    Add tests for arrays

---
+++ test/solc-verify/ArraysMem.sol
@@ -0,0 +1,24 @@
+pragma solidity >=0.5.0;
+
+contract ArraysMem {
+    function() external payable {
+        int[] memory a1 = new int[](2);
+        a1[0] = 1;
+        a1[1] = 2;
+        assert(a1[0] == 1);
+        assert(a1[1] == 2);
+
+        int[] memory a2 = a1; // Reference copy
+        assert(a1[0] == 1);
+        assert(a1[1] == 2);
+        assert(a2[0] == 1);
+        assert(a2[1] == 2);
+
+        a2[0] = 3; // Changes original as well
+
+        assert(a1[0] == 3);
+        assert(a1[1] == 2);
+        assert(a2[0] == 3);
+        assert(a2[1] == 2);
+    }
+}
