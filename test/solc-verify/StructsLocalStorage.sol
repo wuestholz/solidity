94a289bbe5dc9ff158d48cdee25f6972ad76ff65
Akos Hajdu <akos.hajdu@sri.com>
2019-07-30 13:31:53 -0400

    Extend test case

--- test/solc-verify/StructsLocalStorage.sol
+++ test/solc-verify/StructsLocalStorage.sol
@@ -11,54 +11,71 @@ contract StructsLocalStorage {
     }
 
     T t1;
-    S ss;
+    S s1;
+    S s2;
 
     function testSimple() public {
-        ss.x = 1;
-        ss.t.z = 2;
-        assert(ss.x == 1);
-        assert(ss.t.z == 2);
+        s1.x = 1;
+        s1.t.z = 2;
+        assert(s1.x == 1);
+        assert(s1.t.z == 2);
 
-        S storage sl = ss;
+        S storage sl = s1;
         assert(sl.x == 1);
         assert(sl.t.z == 2);
 
-        ss.x = 3;
-        ss.t.z = 4;
+        s1.x = 3;
+        s1.t.z = 4;
         assert(sl.x == 3);
         assert(sl.t.z == 4);
 
         sl.x = 5;
         sl.t.z = 6;
-        assert(ss.x == 5);
-        assert(ss.t.z == 6);
+        assert(s1.x == 5);
+        assert(s1.t.z == 6);
     }
 
     function testMember() public {
-        ss.x = 1;
-        ss.t.z = 2;
+        s1.x = 1;
+        s1.t.z = 2;
         t1.z = 3;
-        assert(ss.x == 1);
-        assert(ss.t.z == 2);
+        assert(s1.x == 1);
+        assert(s1.t.z == 2);
         assert(t1.z == 3);
 
-        T storage tl = ss.t;
+        T storage tl = s1.t;
         tl.z = 4;
 
-        assert(ss.x == 1);
-        assert(ss.t.z == 4);
+        assert(s1.x == 1);
+        assert(s1.t.z == 4);
         assert(t1.z == 3);
 
         tl = t1;
         tl.z = 5;
 
-        assert(ss.x == 1);
-        assert(ss.t.z == 4);
+        assert(s1.x == 1);
+        assert(s1.t.z == 4);
         assert(t1.z == 5);
+    }
+
+    function testConditional(bool b) public {
+        // Set three members to 1
+        t1.z = 1;
+        s1.t.z = 1;
+        s2.t.z = 1;
+        // Conditionally point to one of the first two
+        T storage ptr = t1;
+        if (b) ptr = s1.t;
+        ptr.z = 2; // Change
+
+        // The third one should not change regardless condition
+        assert(s2.t.z == 1);
     }
 
     function() external payable {
         testSimple();
         testMember();
+        testConditional(true);
+        testConditional(false);
     }
 }
