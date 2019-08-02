e09b7a871067f8fc8951e4a0bbe3b4e641db3289
Dejan Jovanovic <dejan.jovanovic@sri.com>
2019-08-02 12:01:32 -0400

    test case

---
+++ test/solc-verify/LocalStorageSpec.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.5.0;
+
+contract A {
+  struct S {
+      int x;
+  }
+  function setA0(S storage s_ptr) internal {
+    s_ptr.x = 0;
+  }
+}
+
+contract B is A {
+  S s;
+  /// @notice postcondition s.x == 0
+  function setB0(S storage s_ptr) internal {
+    setA0(s_ptr);
+  }
+}
+
+contract C is B {
+  S s;
+  function setC0(S storage s_ptr) internal {
+    setB0(s_ptr);
+  }
+  function() external payable {
+    setC0(s);
+    assert(s.x == 0);
+  }
+
+}
