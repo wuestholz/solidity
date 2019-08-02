40db500d64f2a85d30d50d8c8c460e750b36bd9d
Akos Hajdu <akos.hajdu@sri.com>
2019-08-02 11:10:00 -0400

    Extend example on partial results

--- test/solc-verify/Partial.sol
+++ test/solc-verify/Partial.sol
@@ -1,15 +1,25 @@
 pragma solidity >=0.5.0;
 
 contract Partial {
+    uint x;
+    uint y;
 
-    /// @notice postcondition r == x + 1
-    function cannotverify(uint x) public pure returns (uint r) {
-        assembly {}
-        return x + 1;
+    /// @notice modifies x
+    /// @notice postcondition x == __verifier_old_uint(x) + 1
+    function unsupported() internal {
+        assembly {
+            let t := sload(x_slot)
+            t := add(t, 1)
+            sstore(x_slot, t)
+        }
     }
 
-    function f() public pure {
-        uint x = cannotverify(5);
-        assert(x == 6);
+    /// @notice modifies x
+    /// @notice modifies y
+    function() external payable {
+        x = 1;
+        y = 2;
+        unsupported();
+        assert(x == y);
     }
 }
