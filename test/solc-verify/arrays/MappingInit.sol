0e0eeeb2c7bf2c63d2d46e509cf969a38268a4e9
Akos Hajdu <akos.hajdu@sri.com>
2019-08-06 13:20:37 -0400

    Fix/extend example

--- test/solc-verify/arrays/MappingInit.sol
+++ test/solc-verify/arrays/MappingInit.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 /// @notice invariant m[0] == 0
 /// @notice invariant nested[0][0] == 0
 /// @notice invariant ms[0].x == 0
-/// @notice __verifier_eq(m, m2)
+/// @notice invariant __verifier_eq(m, m2)
 contract Test {
     struct S { int x; }
 
@@ -11,4 +11,8 @@ contract Test {
     mapping(int => int) m2;
     mapping(int => mapping(int => int)) nested;
     mapping(int => S) ms;
+
+    constructor() public {
+        assert(m[0] == m2[0]);
+    }
 }
