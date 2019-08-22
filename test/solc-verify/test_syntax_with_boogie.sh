ae49c21cb1234487aee5696d08cff6ef00ef256b
Akos Hajdu <hajdua@mit.bme.hu>
2019-08-22 05:34:14 -0700

    Count segfaults in syntax test

--- test/solc-verify/test_syntax_with_boogie.sh
+++ test/solc-verify/test_syntax_with_boogie.sh
@@ -144,6 +144,7 @@ echo "failures:"
 printf -- '%s\n' "${FAILED_TESTS[@]}" | grep -e "solc-verify error:.*" -o | sort | uniq -c
 printf -- '%s\n' "${FAILED_TESTS[@]}" | grep -e "solc-verify.*exception:.*" -o | sort | uniq -c
 printf -- '%s\n' "${FAILED_TESTS[@]}" | grep -e ".bpl\(.*\): Error" | grep -e ": Error:.*" -o | sort | uniq -c
+printf -- '%s\n' "${FAILED_TESTS[@]}" | grep -e "Segmentation fault (core dumped)" -o | sort | uniq -c
 echo
 echo "warnings:"
 printf -- '%s\n' "${FAILED_TESTS[@]}" | grep -e "solc-verify warning: .*" -o | sort | uniq -c
