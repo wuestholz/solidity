7607a915f2c22325b17051d054019b2099ccaf47
Akos Hajdu <hajdua@mit.bme.hu>
2019-08-26 20:10:26 +0200

    Fix trailing whitespace

--- test/solc-verify/transactions/InternalMsgValue.sol
+++ test/solc-verify/transactions/InternalMsgValue.sol
@@ -5,7 +5,7 @@ contract InternalMsgValue {
     function f() payable public returns(uint r) {
         return msg.value;
     }
-    
+
     function() external payable {
         assert(this.f() == 0);    // External call, msg.value is 0
         assert(f() == msg.value); // Internal call, msg.value is forwarded
