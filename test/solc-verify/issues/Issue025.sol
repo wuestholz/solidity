8398038cda5b79e1078604fe0aabd116d6232b8a
Akos Hajdu <hajdua@mit.bme.hu>
2019-08-26 09:32:01 -0700

    Truffle test for issue025

--- test/solc-verify/issues/Issue025.sol
+++ test/solc-verify/issues/Issue025.sol
@@ -1,7 +1,6 @@
 pragma solidity >=0.5.0;
 
 library L {
-    /// @notice 
     function msgsender() internal view returns(address) {
         return msg.sender;
     }
@@ -11,7 +10,7 @@ library L {
 }
 
 contract Issue025 {
-    function f() external payable {
+    function() external payable {
         assert(L.msgsender() == msg.sender);
         assert(L.msgvalue() == msg.value);
     }
